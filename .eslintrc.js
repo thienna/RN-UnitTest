47d95b197a9c84aca007689081760df78fe86bdf
nguyen.anh.thien <nguyen.anh.thien@test.local>
2019-07-17 15:04:16 +0700

    First Test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  "rules": {
+    // enable additional rules
+    "quotes": ["error", "single"],
+    "prettier/prettier": ["off", {
+      "singleQuote": true,
+      "trailingComma": "all",
+    }]
+  },
 };
--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -10,5 +10,9 @@ import App from '../App';
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const tree = renderer.create(
+    <App />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+  // renderer.create(<App />);
 });
